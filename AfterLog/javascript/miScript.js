8d1d2a7d429a030a65846fa37d22b0c39dae350b
sasguel <miguel1994>
2015-10-02 16:46:42 +0200

    last

---
+++ AfterLog/javascript/miScript.js
@@ -0,0 +1,14 @@
+function crearCarpeta(){
+	var fso = new ActiveXObject("Scripting.FileSystemObject");
+	var a = fso.CreateFolder("./images");
+}
+function guardarImagen(){
+
+}
+function dirigirAAprender(){
+	window.location="aprender.html";
+}
+
+function dirigirAAgregar(){
+	window.location="agregar.html"
+}
